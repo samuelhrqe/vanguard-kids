d12883691bb4e39dc7cb9cb81043628a320f3211
isaac <isaacvinicius2121@alu.ufc.br>
2026-06-20 16:40:02 -0300

    feat: implement static status UI for seat occupancy
    add fragment_status layout using ConstraintLayout guidelines to match
    the prototype's 20% container. create StatusFragment class and integrate
    it into MainActivity. prepare UI for future dynamic data binding

---
+++ android/VanGuardKids/app/src/main/java/br/unicamp/iot/vanguardkids/ui/fragment/StatusFragment.kt
@@ -0,0 +1,7 @@
+package br.unicamp.iot.vanguardkids.ui.fragment
+
+import androidx.fragment.app.Fragment
+import br.unicamp.iot.vanguardkids.R
+
+class StatusFragment : Fragment(R.layout.fragment_status) {
+}
